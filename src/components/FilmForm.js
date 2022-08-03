10be5cee096f7e41b1affadadd09fd4b2245ea23
JavGog <javierggjavier@gmail.com>
2022-08-02 18:28:56 -0700

    ready Film.js file

---
+++ src/components/FilmForm.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Film = ({author, children}) => {
+
+    return(
+    <>
+        <h4>{children}</h4>
+        <p>{author}</p>
+    </>
+     )
+
+}
+
+export default Film;
