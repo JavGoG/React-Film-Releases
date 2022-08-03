a47e2bdee583b09c8b6555922f46269b99126e90
JavGog <javierggjavier@gmail.com>
2022-08-02 18:49:01 -0700

    FilmForm.js file ready

--- src/components/FilmForm.js
+++ src/components/FilmForm.js
@@ -1,14 +1,72 @@
-import React from "react";
+import React, {useState} from "react";
 
-const Film = ({author, children}) => {
+const FilmForm = ({onFilmSubmit}) => {
 
-    return(
-    <>
-        <h4>{children}</h4>
-        <p>{author}</p>
-    </>
-     )
+    const [id, setId] = useState("");
+    const [name, setName] = useState("");
+    const [url, setUrl] = useState("");
+
+
+    const handleIdChange = (evt) => {
+    setId(evt.target.value);
+    }
+
+    const handleNameChange = (evt) => {
+    setName(evt.target.value);
+    }
+
+    const handleUrlChange = (evt) => {
+        setName(evt.target.value);
+    }
+
+    const handleFilmSubmit = (evt) => {
+    evt.preventDefault();
+    const idToSubmit = id.trim();
+    const nameToSubmit = name.trim();
+    const urlToSubmit = url.trim();
+
+    if (!idToSubmit || !nameToSubmit){
+        return
+    }
+
+    onFilmSubmit({
+        author: idToSubmit,
+        name: nameToSubmit,
+        url: urlToSubmit
+    });
+
+    setId("");
+    setName("");
+    setUrl("")
+    }
+
+    return (
+    <form onSubmit={handleFilmSubmit}>
+        <input 
+        type="name"
+        placeholder="The id Film"
+        value={id}
+        onChange={handleIdChange}
+        />
+        <input 
+            type="name"
+            placeholder="Title of the film"
+            value={name}
+            onChange={handleNameChange}
+        />
+        <input 
+            type="name"
+            placeholder="Url"
+            value={name}
+            onChange={handleUrlChange}
+        />
+        <input 
+            type="submit"
+            value="Post"
+        />
+    </form>
+)
 
 }
 
-export default Film;
+export default FilmForm;
